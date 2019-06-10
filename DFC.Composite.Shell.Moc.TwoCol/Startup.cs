84326eca464e645a99dfedad45fd85bd18210d6e
Ian Crisp <icrisp@program-systems.co.uk>
2019-06-10 15:29:32 +0100

    Added robots.txt functionality

--- DFC.Composite.Shell.Moc.TwoCol/Startup.cs
+++ DFC.Composite.Shell.Moc.TwoCol/Startup.cs
@@ -86,6 +86,13 @@ namespace DFC.Composite.Shell.Moc.TwoCol
                     defaults: new { controller = "Sitemap", action = "Sitemap" }
                 );
 
+                // add the robots.txt route
+                routes.MapRoute(
+                    name: "Robots",
+                    template: "Robots.txt",
+                    defaults: new { controller = "Robot", action = "Robot" }
+                );
+
                 // add the default route
                 routes.MapRoute(
                     name: "default",
